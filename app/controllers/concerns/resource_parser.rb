208ff2266ad296292a278a2ed402562a5438a5e9
wyxpku <wang_yu_xiang@126.com>
2014-08-01 15:55:04 +0800

    资源显示优化

--- app/controllers/concerns/resource_parser.rb
+++ app/controllers/concerns/resource_parser.rb
@@ -5,8 +5,8 @@ extend ActiveSupport::Concern
 	include REXML
 
 	private
-
-	def SensorParse xml
+    # Sensor
+	def sensor_parse xml
 		a = Hash.new
 		doc = Document.new(xml)
 		doc.elements.each('sml:SensorML/sml:Observation/swe:DataRecord/swe:field/*') do |ele|
@@ -17,12 +17,49 @@ extend ActiveSupport::Concern
 		a
 	end
 
-	def ActuatorParse xml
+	def sensor_identifier xml
+		a = Hash.new
+		doc = Document.new(xml)
+		doc.elements.each("sml:SensorML/sml:Identification/sml:IdentifierList/sml:identifier") do |ele|
+			a[(ele.attribute :name).to_s] = ele.get_elements("sml:Term/sml:value").first.text
+		end
+		a	
+	end
+
+	def sensor_description xml
+		a = Hash.new
+		doc = Document.new(xml)
+		doc.elements.each("sml:SensorML/sml:Capability/swe:DataRecord/swe:field/swe:Text") do |ele|
+			a[ele.get_elements("gml:description").first.text.to_s] = ele.get_elements("swe:value").first.text
+		end
+		a
+	end
+	# Actuator
+	def actuator_parse xml
 		a = Hash.new
 		doc = Document.new(xml)
 		doc.elements.each('sml:SensorML/sml:Parameter/swe:DataRecord/swe:field/*') do |ele|
 			a[(ele.attribute :name).to_s] = ele.get_elements("gml:description").first.text
-			end
+		end
 		a
 	end
+
+	def actuator_identification xml
+		a = Hash.new
+	    doc = Document.new(xml)
+	    doc.elements.each("sml:SensorML/sml:Identification/sml:IdentifierList/sml:identifier") do |ele|
+	        a[(ele.attribute :name).to_s] = ele.get_elements("sml:Term/sml:value").first.text
+	    end
+	    a
+	end
+
+	def actuator_description xml
+		a = Hash.new
+		doc = Document.new(xml)
+		doc.elements.each("sml:SensorML/sml:Capability/swe:Text") do |ele|
+			a[ele.get_elements("gml:description").first.text.to_s] = ele.get_elements("swe:value").first.text
+		end
+		a
+	end
+
 end
